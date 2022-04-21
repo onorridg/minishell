24c4e8e5a3193dbc4cd600dc148fe08e527f1d24
onorridg <yatruesoy@gmail.com>
2022-04-21 15:32:07 +0300

    fix echo parser

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: onorridg <onorridg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 19:05:07 by onorridg          #+#    #+#             */
-/*   Updated: 2022/04/21 14:13:21 by onorridg         ###   ########.fr       */
+/*   Updated: 2022/04/21 15:30:01 by onorridg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,19 +63,30 @@ int	ft_echo(t_command *command)
 		{	
 			flag = 0;
 			i = 0;
-			if (command->command_parts[j][i] == '"' || command->command_parts[j][i] == '\'')
-				flag = command->command_parts[j][i++];
 			replace_variable = TRUE;
 			if (flag == '\'')
 				replace_variable = FALSE;
 			while (command->command_parts[j][i])
-			{
-				if (command->command_parts[j][i] == '$' && replace_variable)
+			{	
+				if (command->command_parts[j][i] == '"' || command->command_parts[j][i] == '\'')
+				{
+					if (flag == 0)
+						flag = command->command_parts[j][i];
+					else if (command->command_parts[j][i] == flag)
+						flag = 0;
+					else
+						write(1, &command->command_parts[j][i], 1);
+					if (flag == '"' || flag == 0)
+						replace_variable = TRUE;
+					else 
+						replace_variable = FALSE;
+				}
+				else if (command->command_parts[j][i] == '$' && replace_variable)
 					i += print_variable(&command->command_parts[j][i]);
 				else 
 					write(1, &command->command_parts[j][i], 1);
 				i++;
-				if (command->command_parts[j][i] == flag)
+				if (!command->command_parts[j][i + 1] && command->command_parts[j][i] == flag)
 					break;
 			}
 			j++;
