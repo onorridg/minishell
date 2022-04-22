fe6dc148888cc5c81b7d44293601be091df6572f
onorridg <yatruesoy@gmail.com>
2022-04-22 17:03:35 +0300

    some fix

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: onorridg <onorridg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 19:05:07 by onorridg          #+#    #+#             */
-/*   Updated: 2022/04/22 16:20:36 by onorridg         ###   ########.fr       */
+/*   Updated: 2022/04/22 17:03:19 by onorridg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	print_variable(char *string)
 {
-	int 	variable_len;
+	int		variable_len;
 	char	*variable;
 	char	*value;
 
@@ -27,61 +27,70 @@ int	print_variable(char *string)
 	return (variable_len);
 }
 
+int	miss_flag(int *j, t_command *command, int *newline)
+{
+	if (ft_strcmp(command->command_parts[*j], "-n"))
+	{
+		*j = 2;
+		*newline = FALSE;
+		while (ft_strcmp(command->command_parts[*j], "-n"))
+			*j += 1;
+	}
+	return (*j);
+}
+
+void	quotes(char ch, char *flag, int *replace_variable)
+{
+	if (*flag == 0)
+		*flag = ch;
+	else if (ch == *flag)
+		*flag = 0;
+	else
+		write(1, &ch, 1);
+	if (*flag == '"' || *flag == 0)
+		*replace_variable = TRUE;
+	else
+		*replace_variable = FALSE;
+}
+
+void	print_one_word(char *word, int flag, int i)
+{
+	int	replace_variable;
+
+	replace_variable = TRUE;
+	if (flag == '\'')
+		replace_variable = FALSE;
+	while (word[i])
+	{	
+		if (word[i] == '"' || word[i] == '\'')
+			quotes(word[i], &flag, &replace_variable);
+		else if (word[i] == '$' && replace_variable)
+			i += print_variable(&word[i]);
+		else
+			write(1, &word[i], 1);
+		i++;
+		if (!word[i + 1] && word[i] == flag)
+			break ;
+	}	
+}
 
 int	ft_echo(t_command *command)
 {
-	int	i;
-	int j;
-	int newline;
-	int replace_variable;
-	char flag;
-	
+	int	j;
+	int	newline;
+
 	j = 1;
 	newline = TRUE;
-	if (command->command_parts[1])
+	if (command->command_parts[j])
 	{
-		if (ft_strcmp(command->command_parts[1], "-n"))
+		j = miss_flag(&j, command, &newline);
+		while (command->command_parts[j])
 		{
-			j = 2;
-			newline = FALSE;
-			while(ft_strcmp(command->command_parts[j], "-n"))
-				j++;
-		}
-		while(command->command_parts[j])
-		{	
-			flag = 0;
-			i = 0;
-			replace_variable = TRUE;
-			if (flag == '\'')
-				replace_variable = FALSE;
-			while (command->command_parts[j][i])
-			{	
-				if (command->command_parts[j][i] == '"' || command->command_parts[j][i] == '\'')
-				{
-					if (flag == 0)
-						flag = command->command_parts[j][i];
-					else if (command->command_parts[j][i] == flag)
-						flag = 0;
-					else
-						write(1, &command->command_parts[j][i], 1);
-					if (flag == '"' || flag == 0)
-						replace_variable = TRUE;
-					else 
-						replace_variable = FALSE;
-				}
-				else if (command->command_parts[j][i] == '$' && replace_variable)
-					i += print_variable(&command->command_parts[j][i]);
-				else 
-					write(1, &command->command_parts[j][i], 1);
-				i++;
-				if (!command->command_parts[j][i + 1] && command->command_parts[j][i] == flag)
-					break;
-			}
+			print_one_word(command->command_parts[j], 0, 0);
 			j++;
 			if (command->command_parts[j])
-				write(1, " ", 1);	
+				write(1, " ", 1);
 		}
-		
 	}
 	if (newline)
 		write(1, "\n", 1);
